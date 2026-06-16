0d5cc78c12fd1b5164f33e0f14cbd81845335a58
dlgus-cell <b01028328551@gmail.com>
2026-06-17 08:38:55 +0900

    Delete GimmickRecord.cs

--- GimmickRecord.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-/// <summary>기믹 하나의 저장 상태. saveId(고유) → state(문자열) 매핑.</summary>
-[Serializable]
-public class GimmickRecord
-{
-    public string saveId;
-    public string state;
-}
